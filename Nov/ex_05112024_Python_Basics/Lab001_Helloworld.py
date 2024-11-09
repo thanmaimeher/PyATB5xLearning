9f743520dd215b26a260cd9201cf9c9a96e8b1e2
thanmai <thanmaimeher@gmail.com>
2024-11-09 08:22:33 +0530

    myfirst container

---
+++ Nov/ex_05112024_Python_Basics/Lab001_Helloworld.py
@@ -0,0 +1,31 @@
+print("Hello Wrold")
+
+#Heart Symbol used in python
+print("\U00002764")
+
+#we can append this symbol
+print("Hello world!","\U00002764")
+
+#We can also have emojis through programing in python
+print("\U0001F601")  # Grinning Face with Smiling Eyes
+print("\U0001F607")  # Smiling Face with Halo
+print("\U0001F60D")  # Smiling Face with Heart-Eyes
+
+#Hello World example with emoji and heart
+print("Hello World!\U00002764\U0001F601\U0001F607\U0001F60D")
+
+#Here \ indicates or specifies python interpreter that
+# after \ don't consider it as string but refer it as command
+#We can pass multiple arguments to print function
+print("Hello","Everyone,", "How are you?" )
+
+#In the above line output we didn't provide  spaces between the arguments
+#but print function did it automatically
+#Now we would be writing in multiple lines using print function
+print("Hi People,");\
+    print("How are you all?");\
+        print("I am Fine\U0001F601")
+
+#Shotcut to comment any selected code is ctrl+/
+
+
